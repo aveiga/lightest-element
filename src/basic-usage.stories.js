0ed53d21d37cfdd4ec77757e82e5bb4ff30b95a5
André Branco Veiga <a.branco.veiga@gmail.com>
2019-12-09 23:08:07 +0000

    adding storybook

---
+++ src/basic-usage.stories.js
@@ -0,0 +1,11 @@
+import { LightestElement } from "./dist/lightes-element.js";
+
+export default { title: "Button" };
+
+export const withText = () => '<button class="btn">Hello World</button>';
+
+export const withEmoji = () => {
+  const button = document.createElement("button");
+  button.innerText = "😀 😎 👍 💯";
+  return button;
+};
